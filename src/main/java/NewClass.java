a220725d00907f753f0dcf11a295a3ca16ea7ef7
User1 <User1@10.250.12.193>
2017-01-18 12:24:11 -0500

    Initial Submission

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User1
+ */
+public class NewClass {
+    
+    public static void main (String[]){
+    System.out.println("HELLO WORLD");
+    }
+}
